db9a0582c976b4f5bc4ee81c89a938a9633252cc
andreas <mail@a-voss.de>
2020-09-05 20:39:55 +0200

    uuid failed with 10.000

--- src/test/java/de/jlab/scales/UtilsTest.java
+++ src/test/java/de/jlab/scales/UtilsTest.java
@@ -12,7 +12,7 @@ public class UtilsTest {
 
   @Test
   public void testUuid() {
-    final int N = 10000;
+    final int N = 1000;
     Set<String> set = new HashSet<>();
     for (int i = 0; i < N; i++) {
       set.add(Utils.uuid());
