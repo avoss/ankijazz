a0e103e9781f7040ac48cae4ec9be046e230a394
andreas <mail@a-voss.de>
2020-11-09 20:26:20 +0100

    need to escape html in csv

--- src/test/java/de/jlab/scales/anki/MustacheCardTest.java
+++ src/test/java/de/jlab/scales/anki/MustacheCardTest.java
@@ -5,24 +5,26 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MustacheCardTest {
-  
+
   static class TestModel implements HasDifficulty {
-    
-    private final String front = "Question";
-    private final String back = "Answer";
+
+    private final String front = "A < B?";
+    private final String back = "Yes";
     private final String tags = "A B";
 
     @Override
     public int getDifficulty() {
       return 0;
     }
-    
+
     public String getFront() {
       return front;
     }
+
     public String getBack() {
       return back;
     }
+
     public String getTags() {
       return tags;
     }
@@ -31,7 +33,7 @@ public class MustacheCardTest {
   @Test
   public void testToCsv() {
     MustacheCard<TestModel> card = new MustacheCard<>(new TestModel(), "MustacheCard");
-    assertEquals("<div> This is the <b>Question</b> </div>;<div> Cool, an <b>Answer</b> </div>;A B", card.toCsv());
+    assertEquals("<div> Question: <b>A &lt;B?</b> </div>;<div> Answer: <b>Yes</b> </div>;A B", card.toCsv());
   }
 
 }
