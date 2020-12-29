d9e24c4ddb6e77450e4efee5fbc905b585bd4004
andreas <mail@a-voss.de>
2020-12-29 14:01:26 +0100

    Progression Parser finished

---
+++ src/main/java/de/jlab/scales/midi/song/ProgressionParser.java
@@ -0,0 +1,169 @@
+package de.jlab.scales.midi.song;
+
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.CHORD;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.END_CHOICE;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.END_SEQUENCE;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.EOF;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.START_CHOICE;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.START_SEQUENCE;
+import static java.util.stream.Collectors.toList;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import de.jlab.scales.Utils;
+import de.jlab.scales.theory.ChordParser;
+import de.jlab.scales.theory.KeySignature;
+import de.jlab.scales.theory.Scale;
+/*
+ * 
+ * maj triads: "A D G C"
+ * min triads: "Am Dm G C"
+ * dim triads: "Ao Do G C"
+ * aug triads: "Am Do G+ Cm"
+ * all triads: "Am Do G+ (Cm C)"
+ * 
+ * blues: "A7 D7 A7 A7 D7 D7 A7 A7 E7 D7 A7 E7" KeyOf A! or D?
+ * jazzblues: "A7 D9 A7 [A7sus4 A7#5] D9 D#dim7 A7 Gb7#5 E7#5#9 [A9 F#7#5] [Bm9 (E7#5#9, E7#5b9)]"
+ * 
+ * focus on m7, 7, maj7: "Am7 Dm7 G7 Cmaj7"
+ * focus on m7b5, 7b9  : "F#mb5 Bmb5 E7b9 Am7"
+ * focus on m6         : "F#m6 Bm6 E7b9 Am7"
+ * focus on m6         : "F#m6 Bm6 E7b9 Am7"
+ * 
+ */
+public class ProgressionParser {
+  
+  private Scanner scanner;
+
+  static class Scanner {
+    public enum  Token { BOF, START_CHOICE, END_CHOICE, START_SEQUENCE, END_SEQUENCE, CHORD, EOF }
+
+    private static final Pattern PATTERN = Pattern.compile("\\s*(\\()|(\\))|(\\[)|(\\])|([^\\s\\(\\)\\[\\]]+)", Pattern.CASE_INSENSITIVE);
+    private Matcher matcher;
+    private Token token = Token.BOF;
+    private String value;
+
+    public Scanner(String string) {
+      matcher = PATTERN.matcher(string);
+    }
+
+    public Token next() {
+      if (token == EOF) {
+        return token;
+      }
+      if (!matcher.find()) {
+        token = EOF;
+        return token;
+      }
+      value = matcher.group(0);
+      if (matcher.group(1) != null) {
+        token = START_CHOICE;
+      } else if (matcher.group(2) != null) {
+        token = END_CHOICE;
+      } else if (matcher.group(3) != null) {
+        token = START_SEQUENCE;
+      } else if (matcher.group(4) != null) {
+        token = END_SEQUENCE;
+      } else if (matcher.group(5) != null) {
+        token = CHORD;
+      } else {
+        throw new IllegalStateException();
+      }
+      return token;
+    }
+
+    public Token getToken() {
+      return token;
+    }
+    
+    public String getValue() {
+      return value;
+    }
+
+  }
+
+  public List<ChordFactory> parse(String progression) {
+    scanner = new Scanner(progression);
+    return recurse();
+  }
+
+  private List<ChordFactory> recurse() {
+    List<ChordFactory> list = new ArrayList<>();
+    while (true) {
+      switch (scanner.next()) {
+      case CHORD:
+        list.add(new SimpleChordFactory(scanner.getValue()));
+        break;
+      case END_SEQUENCE:
+      case END_CHOICE:
+      case EOF:
+        return list;
+      case START_SEQUENCE:
+        list.add(sequence());
+        break;
+      case START_CHOICE:
+        list.add(choice());
+        break;
+      default: 
+        throw new IllegalStateException("Unknown Token: " + scanner.getToken());
+      }
+    }
+  }
+
+  private ChordFactory choice() {
+    return new ChoiceChordFactory(recurse());
+  }
+
+  private ChordFactory sequence() {
+    return new SequenceChordFactory(recurse());
+  }
+
+  static interface ChordFactory {
+    List<Chord> create(KeySignature keySignature);
+  }
+
+  static class ChoiceChordFactory implements ChordFactory {
+    private Iterator<ChordFactory> iterator;
+    public ChoiceChordFactory(List<ChordFactory> factories) {
+      iterator = Utils.loopIterator(factories);
+    }
+    @Override
+    public List<Chord> create(KeySignature keySignature) {
+      return iterator.next().create(keySignature);
+    }
+  }
+  
+  static class SequenceChordFactory implements ChordFactory {
+    private List<ChordFactory> factories;
+    public SequenceChordFactory(List<ChordFactory> factories) {
+      this.factories = factories;
+    }
+    @Override
+    public List<Chord> create(KeySignature keySignature) {
+      return factories.stream().flatMap(f -> f.create(keySignature).stream()).collect(toList());
+    }
+  }
+  
+  static class SimpleChordFactory implements ChordFactory {
+
+    private Scale scale;
+    private String type;
+    
+    SimpleChordFactory(String string) {
+      scale = ChordParser.parseChord(string);
+      type = string.replaceAll("[A-G][b#]?", "");
+    }
+    
+    @Override
+    public List<Chord> create(KeySignature keySignature) {
+      Scale transposed = scale.transpose(keySignature.getNotationKey().ordinal());
+      String symbol = keySignature.notate(transposed.getRoot()).concat(type);
+      return List.of(Chord.of(transposed, symbol));
+    }
+    
+  }
+}
---
+++ src/test/java/de/jlab/scales/midi/song/ProgressionParserTest.java
@@ -0,0 +1,74 @@
+package de.jlab.scales.midi.song;
+
+import static de.jlab.scales.TestUtils.majorKeySignature;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.BOF;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.CHORD;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.END_CHOICE;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.END_SEQUENCE;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.EOF;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.START_CHOICE;
+import static de.jlab.scales.midi.song.ProgressionParser.Scanner.Token.START_SEQUENCE;
+import static java.util.stream.Collectors.toList;
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+import de.jlab.scales.midi.song.ProgressionParser.ChordFactory;
+import de.jlab.scales.midi.song.ProgressionParser.Scanner;
+import de.jlab.scales.midi.song.ProgressionParser.SimpleChordFactory;
+import de.jlab.scales.theory.KeySignature;
+import de.jlab.scales.theory.Note;
+import de.jlab.scales.theory.Scales;
+
+public class ProgressionParserTest {
+
+  @Test
+  public void testScanner() {
+    Scanner scanner = new Scanner("[(Ab7#5)] CmΔ7 Dm7");
+    assertEquals(BOF, scanner.getToken());
+    assertEquals(START_SEQUENCE, scanner.next());
+    assertEquals(START_CHOICE, scanner.next());
+    assertEquals(CHORD, scanner.next());
+    assertEquals("Ab7#5", scanner.getValue());
+    assertEquals(END_CHOICE, scanner.next());
+    assertEquals(END_SEQUENCE, scanner.next());
+    assertEquals(CHORD, scanner.next());
+    assertEquals("CmΔ7", scanner.getValue());
+    assertEquals(CHORD, scanner.next());
+    assertEquals("Dm7", scanner.getValue());
+    assertEquals(EOF, scanner.next());
+    assertEquals(EOF, scanner.next());
+  }
+
+  @Test
+  public void testSimpleChordFactory() {
+    ChordFactory factory = new SimpleChordFactory("CΔ7");
+    KeySignature keyOfG = majorKeySignature(Note.G);
+    List<Chord> list = factory.create(keyOfG);
+    assertEquals(1, list.size());
+    assertEquals(Chord.of(Scales.Cmaj7.transpose(Note.G), "GΔ7"), list.get(0));
+  }
+  
+  @Test
+  public void testProgressionParser() {
+    assertProgression("Dm7 Em7", "| Em7 | F#m7 |");
+    assertProgression("[Dm7 Em7]", "| Em7 F#m7 |");
+    assertProgression("(Dm7 Em7)", "| Em7 |", "| F#m7 |");
+    assertProgression("(Dm7 (Em7 Fm7))", "| Em7 |", "| F#m7 |", "| Em7 |", "| Gm7 |");
+    assertProgression("(Dm7 [Em7 Fm7])", "| Em7 |", "| F#m7 Gm7 |");
+    assertProgression("Am7 [Dm7 (Em7 Fm7)]", "| Bm7 | Em7 F#m7 |", "| Bm7 | Em7 Gm7 |");
+  }
+
+  private void assertProgression(String progression, String ... songs) {
+    ProgressionParser parser = new ProgressionParser();
+    List<ChordFactory> factories = parser.parse(progression);
+    KeySignature keyOfD = majorKeySignature(Note.D);
+    for (String expected : songs) {
+      List<Bar> bars = factories.stream().map(cf -> cf.create(keyOfD)).map(chords -> new Bar(chords)).collect(toList());
+      Song song = new Song(bars);
+      assertEquals(expected, song.toString());
+    }
+  }
+}
