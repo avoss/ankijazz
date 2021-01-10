37b7ae303a06d2baee7549107d23c5285539cd43
andreas <mail@a-voss.de>
2021-01-10 12:50:31 +0100

    run lily and lame in parallel

--- docker/anki.sh
+++ docker/anki.sh
@@ -1,12 +1,32 @@
-for i in *.ly; do
-	name=$(basename -s '.ly' $i) &&
-	lilypond --png -dresolution=200 "${name}.ly"
-done
+runLily() {
+  name=$(basename -s '.ly' $1) &&
+  lilypond --png -dresolution=200 "${name}.ly"
+}
+export -f runLily
 
-for i in *.midi; do
-  name=$(basename -s '.midi' $i) &&
+find . -name '*.ly' | parallel runLily
+
+runSynth() {
+  name=$(basename -s ".midi" $1) &&
   timidity "${name}.midi" -Ow &&
-  normalize-audio "${name}.wav" &&
-  lame "${name}.wav" "${name}.mp3" &&
+  normalize-audio --quiet "${name}.wav" &&
+  lame --quiet "${name}.wav" "${name}.mp3" &&
   rm "${name}.wav"
-done
+}
+export -f runSynth
+
+find . -name '*.midi' | parallel runSynth
+
+
+#for i in *.ly; do
+#  name=$(basename -s '.ly' $i) &&
+#  lilypond --png -dresolution=200 "${name}.ly"
+#done
+#
+#for i in *.midi; do
+#  name=$(basename -s '.midi' $i) &&
+#  timidity "${name}.midi" -Ow &&
+#  normalize-audio "${name}.wav" &&
+#  lame "${name}.wav" "${name}.mp3" &&
+#  rm "${name}.wav"
+#done
