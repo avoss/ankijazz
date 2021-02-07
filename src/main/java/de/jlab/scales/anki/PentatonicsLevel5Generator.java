3c5fc213715f97b8bdbf339095f79a07e56b7858
andreas <mail@a-voss.de>
2021-02-07 18:17:36 +0100

    fix assertThatChordIsContainedInScale

--- src/main/java/de/jlab/scales/anki/PentatonicsLevel5Generator.java
+++ src/main/java/de/jlab/scales/anki/PentatonicsLevel5Generator.java
@@ -1,17 +1,21 @@
 package de.jlab.scales.anki;
 
+import static de.jlab.scales.theory.ScaleUniverse.MODES;
 import static de.jlab.scales.theory.ScaleUniverse.PENTAS;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Set;
 import java.util.function.Function;
 
 import de.jlab.scales.fretboard2.Marker;
+import de.jlab.scales.theory.BuiltinScaleType;
 import de.jlab.scales.theory.Note;
 import de.jlab.scales.theory.PentatonicChooser;
 import de.jlab.scales.theory.Scale;
 import de.jlab.scales.theory.ScaleInfo;
+import de.jlab.scales.theory.ScaleType;
 import de.jlab.scales.theory.Scales;
 
 public class PentatonicsLevel5Generator extends AbstractFretboardGenerator {
@@ -26,12 +30,19 @@ public class PentatonicsLevel5Generator extends AbstractFretboardGenerator {
     List<ChordScaleAudio> pairs = new ArrayList<>();
     PentatonicChooser chooser = new PentatonicChooser();
     for (Scale scale : Scales.commonModes(false)) {
-//    for (Scale scale : List.of(Scales.CMelodicMinor.superimpose(Note.B))) {
+      if (!scaleContainsPentatonic(scale)) {
+        continue;
+      }
       Scale chord = scale.getChord(0);
       Scale penta = chooser.chooseBest(chord);
       pairs.add(new ChordScaleAudio(penta, scale, chord));
     }
     return pairs;
+  }
+
+  private boolean scaleContainsPentatonic(Scale scale) {
+    ScaleType type = MODES.findFirstOrElseThrow(scale).getScaleType();
+    return Set.of(BuiltinScaleType.Major, BuiltinScaleType.MelodicMinor).contains(type);
   }
   
   @Override
