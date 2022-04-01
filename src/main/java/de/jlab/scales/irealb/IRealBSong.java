9fde5196d63f400a80f37c858824e02af9598ea4
andreas <mail@a-voss.de>
2022-04-01 19:08:28 +0200

    IRealB removed

--- src/main/java/de/jlab/scales/irealb/IRealBSong.java
+++
@@ -1,9 +0,0 @@
-package de.jlab.scales.irealb;
-
-public class IRealBSong {
-  private String title = "Title";
-  private String composer = "Composer";
-  private String style = "Style";
-  private String key = "";
-  private String tune;
-}
